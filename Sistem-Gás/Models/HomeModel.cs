139041d06ef6be7419388abbe77038487778aa0f
johnluki12 <105826693+johnluki12@users.noreply.github.com>
2024-03-12 21:34:43 -0300

    Adicionar arquivos de projeto.

---
+++ Sistem-Gás/Models/HomeModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Sistem_gás.Models
+{
+    public class HomeModel 
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public  string Cpf { get; set; }    
+        public string Endereco { get; set;}
+        public string Telefone  { get; set; } 
+    }
+}
---
+++ Sistem-Gás/Models/UsuarioModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Sistem_gás.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string Endereco { get; set; }   
+        public string telefone  { get; set; } 
+    }
+}
